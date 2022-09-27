03e3f48a974612944a75122abd8e7dd22f05909b
Cin2323 <95028396+Cin2323@users.noreply.github.com>
2022-09-27 18:24:25 +0300

    da

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,84 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './component/add-product/add-product.component';
+import { AdminComponent } from './component/admin/admin.component';
+import { CartComponent } from './component/cart/cart.component';
+import { DetailsComponent } from './component/details/details.component';
+import { FavoriteProductsComponent } from './component/favorite-products/favorite-products.component';
+import { HomeComponent } from './component/home/home.component';
+import { OrderComponent } from './component/order/order.component';
+import { ProductComponent } from './component/product/product.component';
+import { UpdateProductComponent } from './component/update-product/update-product.component';
+import { LoginComponent } from './component/users/login/login.component';
+import { SignUpComponent } from './component/users/sign-up/sign-up.component';
+import { RolexComponent } from './component/watches/men/rolex/rolex.component';
+import { SlazengerComponent } from './component/watches/men/slazenger/slazenger.component';
+import { RolexForWomenComponent } from './component/watches/women/rolex-for-women/rolex-for-women.component';
+import { SlagenderForWomenComponent } from './component/watches/women/slagender-for-women/slagender-for-women.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'categoryFossilForMan/:brand/:gender',
+    component:ProductComponent
+  },
+  {
+    path:'categoryForMan/:brand/:gender',
+    component:RolexComponent
+  },
+  {
+    path:'categoryForWoman/:brand/:gender',
+    component:RolexForWomenComponent
+  },
+  {
+    path:'addProduct',
+    component:AddProductComponent
+  },
+  {
+    path:'product/:productId',
+    component:DetailsComponent
+  },
+  {
+    path:'admin',
+    component:AdminComponent
+  },
+  {
+    path:'editProduct/:productId',
+    component:UpdateProductComponent
+  },
+  {
+    path:'mycart',
+    component:CartComponent
+  },
+  {
+    path:'favoriteProducts',
+    component:FavoriteProductsComponent
+  },
+  {
+    path:'myOrder',
+    component:OrderComponent
+  },
+  {
+    path:'categorySlazengerForMan/:brand/:gender',
+    component:SlazengerComponent
+  },
+  {
+    path:'categorySlazengerForWoman/:brand/:gender',
+    component:SlagenderForWomenComponent
+  },
+  {
+    path:'signUp',
+    component:SignUpComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,78 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ProductComponent } from './component/product/product.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { DetailsComponent } from './component/details/details.component';
+import { AddProductComponent } from './component/add-product/add-product.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AdminComponent } from './component/admin/admin.component';
+import { UpdateProductComponent } from './component/update-product/update-product.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ButtonModule} from 'primeng/button'; 
+import {AccordionModule} from 'primeng/accordion';
+import { CartComponent } from './component/cart/cart.component';
+import { FavoriteProductsComponent } from './component/favorite-products/favorite-products.component';
+import { OrderComponent } from './component/order/order.component';
+import {InputTextModule} from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import {FormsModule} from '@angular/forms';
+import {InputSwitchModule} from 'primeng/inputswitch';
+import { MenubarModule } from 'primeng/menubar';
+import { SlazengerComponent } from './component/watches/men/slazenger/slazenger.component';
+import { LoginComponent } from './component/users/login/login.component';
+import { SignUpComponent } from './component/users/sign-up/sign-up.component';
+import { SlagenderForWomenComponent } from './component/watches/women/slagender-for-women/slagender-for-women.component';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { HomeComponent } from './component/home/home.component';
+import { RolexComponent } from './component/watches/men/rolex/rolex.component';
+import { RolexForWomenComponent } from './component/watches/women/rolex-for-women/rolex-for-women.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductComponent,
+    DetailsComponent,
+    AddProductComponent,
+    AdminComponent,
+    UpdateProductComponent,
+    CartComponent,
+    FavoriteProductsComponent,
+    OrderComponent,
+    SlazengerComponent,
+    LoginComponent,
+    SignUpComponent,
+    SlagenderForWomenComponent,
+    HomeComponent,
+    RolexComponent,
+    RolexForWomenComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    AccordionModule,
+    InputTextModule,
+    DropdownModule,
+    FormsModule,
+    InputSwitchModule,
+    MenubarModule,
+    NgxPaginationModule,
+  
+    
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [CartComponent],
+  bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
